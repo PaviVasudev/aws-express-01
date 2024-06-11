06cd14b5f85bc4abe6ba733d1e4a5c2ab0f6e36e
Pavitra Vasudevan <72784548+PaviVasudev@users.noreply.github.com>
2024-06-11 09:26:07 +0530

    Create print_to_console.py
    Read from file and print

---
+++ print_to_console.py
@@ -0,0 +1,9 @@
+file_path = 'example.txt'
+
+# Opening the file in read mode
+with open(file_path, 'r') as file:
+    # Reading the contents of the file
+    file_contents = file.read()
+
+# Printing the contents to the console
+print(file_contents)
